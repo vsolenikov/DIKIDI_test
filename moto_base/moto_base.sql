6c08e1919682b7d37847370bb58e33d0512e0498
nightprog <solenikov.vova@yandex.ru>
2024-04-03 22:44:49 +0300

    fix: рефакторинг дампа БД и запроса

--- moto_base/moto_base.sql
+++ moto_base/moto_base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Апр 02 2024 г., 21:53
+-- Время создания: Апр 03 2024 г., 21:43
 -- Версия сервера: 10.4.22-MariaDB
 -- Версия PHP: 7.4.27
 
@@ -28,24 +28,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `moto_table` (
-  `moto_id` int(11) NOT NULL,
-  `moto_name` varchar(100) NOT NULL,
-  `retire_status` varchar(20) NOT NULL DEFAULT 'used',
-  `moto_type_id` int(11) NOT NULL
+                              `moto_id` int(11) NOT NULL,
+                              `moto_name` varchar(100) NOT NULL,
+                              `prod_status` tinyint(1) NOT NULL DEFAULT 1,
+                              `moto_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `moto_table`
 --
 
-INSERT INTO `moto_table` (`moto_id`, `moto_name`, `retire_status`, `moto_type_id`) VALUES
-(1, 'Moto_1', 'used', 1),
-(2, 'Moto_2', 'retired', 1),
-(3, 'Moto_3', 'used', 2),
-(4, 'Moto_4', 'used', 2),
-(5, 'Moto_5', 'used', 2),
-(7, 'Moto_6', 'retired', 2),
-(8, 'Moto_7', 'used', 1);
+INSERT INTO `moto_table` (`moto_id`, `moto_name`, `prod_status`, `moto_type_id`) VALUES
+                                                                                     (1, 'Moto_1', 0, 1),
+                                                                                     (2, 'Moto_2', 1, 1),
+                                                                                     (3, 'Moto_3', 0, 2),
+                                                                                     (4, 'Moto_4', 1, 2),
+                                                                                     (5, 'Moto_5', 0, 2),
+                                                                                     (7, 'Moto_6', 4, 2),
+                                                                                     (8, 'Moto_7', 0, 1),
+                                                                                     (9, 'Motorolla', 1, 3),
+                                                                                     (15, 'Motorcycle', 5, 3),
+                                                                                     (16, 'Mottor', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -54,8 +57,8 @@ INSERT INTO `moto_table` (`moto_id`, `moto_name`, `retire_status`, `moto_type_id
 --
 
 CREATE TABLE `moto_types` (
-  `moto_type_id` int(11) NOT NULL,
-  `name_type` varchar(100) NOT NULL
+                              `moto_type_id` int(11) NOT NULL,
+                              `name_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -63,8 +66,9 @@ CREATE TABLE `moto_types` (
 --
 
 INSERT INTO `moto_types` (`moto_type_id`, `name_type`) VALUES
-(1, 'Type_1'),
-(2, 'Type_2');
+                                                           (1, 'Type_1'),
+                                                           (2, 'Type_2'),
+                                                           (3, 'Type_3');
 
 --
 -- Индексы сохранённых таблиц
@@ -74,14 +78,14 @@ INSERT INTO `moto_types` (`moto_type_id`, `name_type`) VALUES
 -- Индексы таблицы `moto_table`
 --
 ALTER TABLE `moto_table`
-  ADD PRIMARY KEY (`moto_id`),
+    ADD PRIMARY KEY (`moto_id`),
   ADD KEY `moto_type_id` (`moto_type_id`);
 
 --
 -- Индексы таблицы `moto_types`
 --
 ALTER TABLE `moto_types`
-  ADD PRIMARY KEY (`moto_type_id`);
+    ADD PRIMARY KEY (`moto_type_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -91,13 +95,13 @@ ALTER TABLE `moto_types`
 -- AUTO_INCREMENT для таблицы `moto_table`
 --
 ALTER TABLE `moto_table`
-  MODIFY `moto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+    MODIFY `moto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT для таблицы `moto_types`
 --
 ALTER TABLE `moto_types`
-  MODIFY `moto_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+    MODIFY `moto_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -107,7 +111,7 @@ ALTER TABLE `moto_types`
 -- Ограничения внешнего ключа таблицы `moto_table`
 --
 ALTER TABLE `moto_table`
-  ADD CONSTRAINT `moto_table_ibfk_1` FOREIGN KEY (`moto_type_id`) REFERENCES `moto_types` (`moto_type_id`);
+    ADD CONSTRAINT `moto_table_ibfk_1` FOREIGN KEY (`moto_type_id`) REFERENCES `moto_types` (`moto_type_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
